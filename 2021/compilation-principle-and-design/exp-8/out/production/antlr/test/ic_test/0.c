1f64cbf838abccf5159bf5cb06d72da647e6fcbf
Hyperzsb <hyperzsb@qq.com>
2021-08-22 15:35:02 +0800

    [refactor]Remove useless compilation output in 2021/compilation-principle-and-design

--- 2021/compilation-principle-and-design/exp-8/out/production/antlr/test/ic_test/0.c
+++
@@ -1,8 +0,0 @@
-int main() {
-	int a, b, c;
-	a = 0;
-	b = 1;
-	c = 2;
-	c = a + b + (c + 3);
-	return 0;
-}
--- 2021/compilation-principle-and-design/exp-8/out/production/antlr/test/semantic_test/4_opnd_not_match.c
+++
@@ -1,7 +0,0 @@
-int main()
-{
-	int a = 2;
-	double b = 3.5;
-	int res = a<<b;
-	return 0;
-}
